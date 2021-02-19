9cdc9f0422cfc3f17d3853416c27722bac669311
leanhtuan <leanhtuan19597@gmail.com>
2021-02-19 13:29:33 +0700

    first commit

---
+++ SystemWarning/assets/style.js
@@ -0,0 +1,12 @@
+// alert('hello mọi người')
+/** nội dung comment
+ * javascript basic
+ * các hàm trong javascript
+ * alert
+ * console
+ * confirm
+ * prompt
+ * set timeout
+ * set interval
+ */
+// console.log("nội dung thông báo log")
